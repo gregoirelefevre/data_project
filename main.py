8917c8d24ef8137116081e4fee5798733875be8c
Gregoire Lefevre <gregoire.lefevre@edu.esiee.fr>
2024-12-13 18:40:01 +0100

    Maj main.py

--- main.py
+++ main.py
@@ -4,16 +4,20 @@ import os
 
 # Set the Kaggle API key using subprocess (ensure the key is correct)
 subprocess.run(['kaggle', 'config', 'set', '-n', 'grgoirelefevre', '-v', '6c63be6eb6226de8fbef6cf36edeed91'])
-
-# Define the target path relative to your project (the 'data/raw' directory)
 target_path = "./data/raw"
-print(target_path)
+
 
 try:
-    # Download the dataset to the specified target path (not the default .cache/kagglehub location)
-    path = kagglehub.dataset_download("piterfm/paris-2024-olympic-summer-games")
-    print("Path to dataset files:", path)
-    subprocess.run(['mv', path, target_path], check=True)
-    print("Move the data to :", target_path)
+    if not(any(os.path.isfile(os.path.join(target_path, f)) for f in os.listdir(target_path))): 
+        path_check = "/Users/gregoirelefevre/.cache/kagglehub/datasets/"
+        if not(any(os.path.isfile(os.path.join(path_check, f)) for f in os.listdir(path_check))):
+            path = kagglehub.dataset_download("piterfm/paris-2024-olympic-summer-games")
+            print("Path to dataset files:", path)
+            subprocess.run(['mv', path, target_path], check=True)
+            print("Move the data to :", target_path)
+        else :
+            print("Move the data to :", path_check)
+    else :
+        print("Already a file in", target_path)
 except Exception as e:
     print("An error occurred:", e)
