c79c942cb0f0de68c2b2e4e178574e2d7788fc92
Gregoire Lefevre <gregoire.lefevre@edu.esiee.fr>
2024-12-18 13:40:15 +0100

    Update Main

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
 from src.get_dataset import getdata 
+from src.dashboard import init_app
+import webbrowser
+import dash
+from dash import dcc, html, Input, Output
+import plotly.express as px
+
 def main():
     getdata()
+    app = dash.Dash(__name__)
+    init_app()
+    webbrowser.open('http://127.0.0.1:8050/')
+    app.run_server(debug=True)
+
 
 
 
